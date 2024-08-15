da66d42ba6dfb42dea1f394d51904d2d0a2b22d1
trry <54043933+tk-425@users.noreply.github.com>
2024-08-14 23:32:01 -0400

    Added updating caller-cli command

--- README.md
+++ README.md
@@ -2,6 +2,19 @@
 
 ## Installation:
 
-```bash
-npm i -g @kt425/caller-cli
-```
+1. Download the repository
+2. Move the repository to the `/usr/local/share` directory
+   ```bash
+   sudo mv /path/to/repository/ /usr/local/share
+   ```
+3. Go to the `/usr/local/share` directory
+4. Make the `caller.js` script executable:
+   ```bash
+   chmod +x caller.js
+   ```
+5. Create a Symlink to your script
+   ```bash
+   sudo ln -x /usr/local/share/caller-cli-main/caller.js /usr/local/bin/caller
+   ```
+6. Close your terminal and restart
+7. Use `caller <command>` to run the Caller-CLI command
---
+++ utils-update.js
@@ -0,0 +1,54 @@
+import inquirer from 'inquirer';
+import { spawn } from 'child_process';
+import {
+  printError,
+  printForceClosedError,
+  printSuccess,
+  printTitle,
+} from './utils-print.js';
+import { GIT_COMMAND } from './utils.js';
+
+export async function update() {
+  printTitle('- Update Caller-CLI -');
+
+  try {
+    const confirmAnswer = await inquirer.prompt([
+      {
+        type: 'confirm',
+        name: 'confirmation',
+        message: 'Are you sure you want to update Caller-CLI?',
+        default: false,
+      },
+    ]);
+
+    if (!confirmAnswer.confirmation) {
+      printError('Update cancelled.');
+      return;
+    }
+
+    const args = [
+      'pull',
+      'origin',
+      'main',
+      '&&',
+      'npm',
+      'update',
+      '&&',
+      'npm',
+      'update',
+    ];
+
+    const process = spawn(GIT_COMMAND, args, { stdio: 'inherit' });
+
+    process.on('close', (code) => {
+      printSuccess('Successfully updated.');
+    });
+
+    process.on('error', (err) => {
+      printError(err.message);
+    });
+  } catch (err) {
+    printForceClosedError(err);
+    return;
+  }
+}
