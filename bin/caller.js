d7bfe302a294bb5f9569aebef5daac46ec729297
trry <54043933+tk-425@users.noreply.github.com>
2024-09-02 15:03:52 -0400

    v1-3-9

--- bin/caller.js
+++ bin/caller.js
@@ -12,6 +12,7 @@ import {
 import { gitListCommands } from '../lib/commands/git.js';
 import { aiCommands, deleteAPIKey } from '../lib/commands/ai.js';
 import { update } from '../lib/commands/update.js';
+import { build } from '../lib/commands/build.js';
 import { VERSION } from '../lib/config.js';
 
 function main() {
@@ -50,6 +51,12 @@ function main() {
     .command('ai')
     .description('AI command')
     .action(() => aiCommands());
+
+  // Custom command build
+  program
+    .command('build')
+    .description('Build custom command')
+    .action(() => build());
 
   // Delete API key
   program
